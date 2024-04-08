e34a7848b1ad4981a4e983aa1b70079df1206934
baxter-io <158606263+baxter-io@users.noreply.github.com>
2024-04-08 16:30:26 +0100

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    document.querySelector('.navlinks').classList.toggle('active');
-});
-// Your JavaScript code, including the toggleMenu() function
-function toggleMenu() {
-    var menuLinks = document.querySelector('.menu-links');
-    menuLinks.classList.toggle('open');
-}
